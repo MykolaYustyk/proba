fb990932c825ba91c9a67d961fe9e28e9a9a71e0
MykolaYustyk <94138609+MykolaYustyk@users.noreply.github.com>
2021-11-20 19:15:34 +0200

    rewrite task5

--- HT_02/task5.py
+++ HT_02/task5.py
@@ -2,8 +2,14 @@
 # 5. Написати скрипт, який залишить в словнику тільки пари із унікальними значеннями (дублікати значень - видалити). 
 # 
 # 
-test_dict =  {'a': 12, 'b' : 15, 'c' : 12, 'd' : 15, 'e' : 1, 'f' : -2}
+test_dict = {'a': 1, 'b': 3, 'c': 1, 'd': 5,'e': [1], 'f': "a", 'g': "a", 'h': [1]}
 print("The original dictionary is : " , test_dict)
-temp_dict = {val : key for key, val in test_dict.items()}
-res_dict = {val : key for key, val in temp_dict.items()}
-print("The dictionary after values removal : ", res_dict) 
+
+temp = []
+result_dict = {}
+for key, val in test_dict.items():
+    if val not in temp:
+        temp.append(val)
+        result_dict[key] = val
+
+print("The dictionary after values removal : " , result_dict)
