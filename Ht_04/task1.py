b736f6451512b1034557a4f4316ebb83288e05e1
Mykola Yustyk <nikola.justik@gmail.com>
2022-05-07 00:36:32 +0300

    rewrite sqrt

--- Ht_04/task1.py
+++ Ht_04/task1.py
@@ -3,8 +3,9 @@
 # 1. Написати функцію < square > , яка прийматиме один аргумент - сторону квадрата, і вертатиме 3 значення (кортеж): периметр квадрата, площа квадрата та його діагональ.
 #
 #
+import math
 def square(side) :
-    result = (4*side, side**2, side * 1.41)
+    result = (4*side, side**2, side * math.sqrt(side))
     return result
 
 side = float(input('Input length of square: '))
